392d19a8692e318a4def30723c82b409ad83ca28
MallombasiMattawang <mattawang.rpl@gmail.com>
2023-02-12 17:51:24 +0800

    first commit

---
+++ src/pages/Event.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { SiHandshake } from "react-icons/si";
+import { RiCalendarEventLine } from "react-icons/ri";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { GiEternalLove } from "react-icons/gi";
+import Iframe from "react-iframe";
+
+const Event = () => {
+  return (
+    <div className="w-full min-h-screen bg-secondary bg-cover bg-center ">
+      <Navbar />
+      <div className="max-w-5xl w-full mx-auto items-center p-5 flex flex-col gap-5">
+        <h1 className="font-shalimar text-primary text-6xl text-center">
+          Susunan Acara
+        </h1>
+        {/* Card */}
+        <div className="w-full flex flex-col md:flex-row gap-8">
+          <div className="w-full h-full flex flex-col md:flex-row">
+            <div className="w-full h-full border-[8px] border-third bg-main bg-top flex flex-col items-center justify-center p-3 rounded-3xl gap-2">
+              <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                <SiHandshake color="white" size={40} />
+              </div>
+              <h1 className="font-Bree text-3xl text-third">AKAD NIKAH</h1>
+              <div
+                className="flex flex-col items-center justify-center text-third font-Open
+               text-center"
+              >
+                <h2>Bertempat Di</h2>
+                <p>Kediaman Mempelai Wanita, Jl. Poros Makassar - Pare Mandalle Utara Pangkep</p>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center text-black font-Open">
+                <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                  <RiCalendarEventLine color="white" size={40} />
+                </div>
+                <p>
+                  Rabu <br />
+                  01 Maret 2023
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center text-black font-Open">
+                <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                  <AiOutlineClockCircle color="white" size={40} />
+                </div>
+                <p>11.00 WITA</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-full flex flex-col md:flex-row">
+            <div className="w-full h-full border-[8px] border-third bg-main bg-top flex flex-col items-center justify-center p-3 rounded-3xl gap-2">
+              <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                <GiEternalLove color="white" size={40} />
+              </div>
+              <h1 className="font-Bree text-3xl text-third">RESEPSI</h1>
+              <div
+                className="flex flex-col items-center justify-center text-third font-Open
+               text-center"
+              >
+                <h2>Bertempat Di</h2>
+                <p>Kediaman Mempelai Wanita, Jl. Poros Makassar - Pare Mandalle Utara Pangkep</p>
+              </div>
+              <div className="flex flex-col items-center justify-center text-center text-black font-Open">
+                <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                  <RiCalendarEventLine color="white" size={40} />
+                </div>
+                <p>
+                  12.00 WITA <br /> s/d Selesai
+                </p>
+              </div>
+              {/* <div className="flex flex-col items-center justify-center text-center text-black font-Open">
+                <div className="w-20 h-20 bg-primary rounded-full flex items-center justify-center">
+                  <AiOutlineClockCircle color="white" size={40} />
+                </div>
+                <p>24.00 WIB</p>
+              </div> */}
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-full flex flex-col md:flex-row mb-24">
+          <div className="w-full h-full border-[8px] border-third bg-main bg-cover bg-top flex flex-col items-center justify-center p-3 rounded-3xl gap-2">
+            <h1 className="font-Bree text-3xl text-third">LOKASI</h1>
+            <Iframe
+              src="https://maps.google.com/maps?q=Jl. Poros Makassar - Pare Mandalle Utara Pangkep&t=&z=10&ie=UTF8&iwloc=&output=embed"
+              className="rounded-xl w-full h-96"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Event;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { useTimer } from "reactjs-countdown-hook";
+
+const Home = () => {
+  const { seconds, minutes, hours, days } = useTimer(1500000, handleTimerFinish);
+
+  function handleTimerFinish() {
+    alert("Waktunya Nikah!");
+  }
+
+  return (
+    <div className="w-screen min-h-screen bg-main bg-cover bg-center bg-no-repeat">
+      <div className="w-full h-full p-8 flex flex-col items-center justify-center gap-5">
+        <h1 className="text-2xl text-primary font-Bree">Undangan Pernikahan</h1>
+        <img src="img/mempelai.png" alt="Mempelai" width={180} className="ml-8" />
+        <div className="flex flex-col gap-1">
+          <h1 className="font-shalimar font-extrabold text-4xl text-primary text-center">
+            Muhammad Irfan Ibnu, S.Kom <br /> & <br /> Irmawati, S.Kom
+          </h1>
+          <div className="bg-primary p-3 rounded-xl text-center">
+          <h2 className="font-Poppins text-white text-center">
+            01 Maret 2023
+          </h2>
+          </div>
+          
+        </div>
+        <div className="flex flex-row text-white font-Bree gap-2">
+          <span className="bg-primary p-5 rounded-xl text-center">
+            {`${days}`} Hari
+          </span>
+          <span className="bg-primary p-5 rounded-xl text-center">
+            {`${hours}`} Jam
+          </span>
+          <span className="bg-primary p-5 rounded-xl text-center">
+            {`${minutes}`} Menit
+          </span>
+          <span className="bg-primary p-5 rounded-xl text-center">
+            {`${seconds}`} Detik
+          </span>
+        </div>
+      </div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
