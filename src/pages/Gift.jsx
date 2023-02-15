e822ca18967a5430d338e7fde0612ce92ca47eb8
muh irfan <74229073+mpampamdev@users.noreply.github.com>
2023-02-15 20:47:15 +0800

    Update Gift.jsx

--- src/pages/Gift.jsx
+++ src/pages/Gift.jsx
@@ -19,21 +19,7 @@ const Gift = () => {
       <Navbar />
       <div className="w-full min-h-screen bg-main bg-cover bg-center p-5 flex flex-col items-center justify-center">
         <div className="max-w-xl w-full bg-white border-[3px] border-third mx-auto rounded-xl flex flex-col items-center justify-center gap-6 p-2">
-          // <img
-          //   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5WSNEo4yCjiHAIZacpGj-wUeuHkv2fKPRrpxj00s0Br_w2fvwq5O8TFVW3GI32YN2Hkw&usqp=CAU"
-          //   alt="Bank"
-          //   width={150}
-          //   height={150}
-          // />
-          <h1 className="text-orange-600">-</h1>
-          <button
-            className={`bg-third ${
-              !copy && "bg-green-600 hover:bg-green-800"
-            } p-3 rounded-md text-white  hover:bg-green-400`}
-            onClick={handleClick}
-          >
-            {copy ? "Copy No.Rek" : "Berhasil diCopy✔️"}
-          </button>
+          <p>Tidak ada lagi hal yang menggembirakan selain kehadiran Anda pada hari bahagia kami. Terimakasih atas doa dan harapan tulus pada kami,.</p>
         </div>
       </div>
     </>
